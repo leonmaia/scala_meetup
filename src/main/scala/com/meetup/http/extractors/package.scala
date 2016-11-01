c2ad4175bc13d714eb33c5a207433f914630d3df
Leon Maia <hi@leonmaia.com>
2016-11-01 21:05:01 +0100

    making throw explicit

--- src/main/scala/com/meetup/http/extractors/package.scala
+++ src/main/scala/com/meetup/http/extractors/package.scala
@@ -1,8 +1,13 @@
 package com.meetup.http
 
+import scala.util.{Failure, Success, Try}
+
 package object extractors {
 
   object UUID {
-    def unapply(arg: String): Option[java.util.UUID] = Option(java.util.UUID.fromString(arg))
+    def unapply(arg: String): Option[java.util.UUID] = Try(java.util.UUID.fromString(arg)) match {
+      case Success(uuid) => Option(uuid)
+      case Failure(e: Exception) => throw e
+    }
   }
 }
